1521705dd44f1eacadaa4b17b2578d02bd7d4b98
In Tak Hwang <sp301415@gmail.com>
2017-05-03 10:45:01 +0900

    Added random partition function.

--- BlottoRecent.py
+++ BlottoRecent.py
@@ -1,14 +1,22 @@
 import random
 import math
 
-TROOP_TOTAL = int(input("Total Troop: "))
-BATTLEFIELD_TOTAL = int(input("Total Battlefield: "))
+
+TROOP_TOTAL = int(input("Total Troops: "))
+BATTLEFIELD_TOTAL = int(input("Total Battlefied: "))
 VALUE_TOTAL = int(input("Total Value: "))
+
+
+def partition(number, part):
+    answer = [0]*part
+    for i in range(0, number):
+        answer[random.randrange(0, part)] += 1
+    return answer
 
 
 def game(troop_profile, value_profile):  # calculates score
     score = [0, 0]
-    for i in range(0, BATTLEFIELD_TOTAL):
+    for i in range(BATTLEFIELD_TOTAL):
         if troop_profile[0][i] > troop_profile[1][i]:
             score[0] += value_profile[0][i]
         elif troop_profile[0][i] == troop_profile[1][i]:  # each gets half of value if tied
@@ -19,19 +27,12 @@ def game(troop_profile, value_profile):  # calculates score
     return score
 
 
-def random_distribute(array):
-
-    # randomly distribute code here
-
-    return array
-
-
 def reduce_improve(troop_profile, player):
     leftover = 0
     opponent = player ^ 1
 
     # reduce oversupply
-    for i in range(0, BATTLEFIELD_TOTAL):
+    for i in range(BATTLEFIELD_TOTAL):
         if troop_profile[player][i] > troop_profile[opponent][i] + 1:
             leftover += troop_profile[player][i] - troop_profile[opponent][i] - 1
             troop_profile[player][i] = troop_profile[opponent][i] + 1
@@ -39,7 +40,7 @@ def reduce_improve(troop_profile, player):
 
     # improve profile
     while leftover > 0:
-        troop_profile[player][random.randint(0, BATTLEFIELD_TOTAL)] += 1
+        troop_profile[player][random.randrange(0, BATTLEFIELD_TOTAL)] += 1
         leftover -= 1
 
     return troop_profile
@@ -47,10 +48,15 @@ def reduce_improve(troop_profile, player):
 
 def main():
     troop_profile = [[0 for col in range(BATTLEFIELD_TOTAL)] for row in range(2)]
+    troop_profile[0] = partition(TROOP_TOTAL, BATTLEFIELD_TOTAL)
+    troop_profile[1] = partition(TROOP_TOTAL, BATTLEFIELD_TOTAL)
     value_profile = [[0 for col in range(BATTLEFIELD_TOTAL)] for row in range(2)]
+    value_profile[0] = partition(VALUE_TOTAL, BATTLEFIELD_TOTAL)
+    value_profile[1] = partition(VALUE_TOTAL, BATTLEFIELD_TOTAL)
     gene_pool = [[0 for col in range(BATTLEFIELD_TOTAL)] for row in range(10)]
 
+    print(troop_profile)
+    print(value_profile)
     print(game(troop_profile, value_profile))
-
 
 main()
